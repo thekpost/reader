4f70b914601e97b71e4532b351b7a2954ad570c6
Ritvvij Parrikh <ritvij.j@gmail.com>
2013-06-02 16:52:55 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedFeed::Application.config.secret_token = '6dcf694aa0eb30987e513c3ed68854a945a9c9994da50ae18c4e14f8657c4adf41d78871c1baa3a0d72164db09df6daa44effa0953c7c5a94279eb732fde25aa'
