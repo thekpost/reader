e522ede523bbf272c89fb78361a03ed6c75ad1ec
Ritvvij Parrikh <ritvij.j@gmail.com>
2013-06-03 00:35:37 +0530

    s

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 RedFeed::Application.routes.draw do
   
-  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks", sessions: "sessions", :registrations => "registrations"} do
-    get '/auth/google_oauth2/callback', to: 'omniauth_callbacks#all'
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks", sessions: "sessions", :registrations => "registrations"}
+  
+  devise_scope :user do
     get "sign_in", :to => "sessions#new", as: :sign_in
     get "sign_up", :to => "devise/registrations#new", as: :sign_up
     get "sign_out", :to => "devise/sessions#destroy", as: :sign_out
+    get '/auth/google_oauth2/callback', to: 'omniauth_callbacks#all'
   end
-    
+  
   resources :users do
     resources :feed_entries
     resources :app_keys do
