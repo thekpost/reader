f2f7e3c2c5be63cc2a6aa9bd73102fae91bfef2c
Ritvvij Parrikh <ritvij.j@gmail.com>
2013-06-03 00:27:10 +0530

    devise issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ RedFeed::Application.routes.draw do
   
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks", sessions: "sessions", :registrations => "registrations"} do
     get '/auth/google_oauth2/callback', to: 'omniauth_callbacks#all'
-  end
-  
-  devise_scope :user do
     get "sign_in", :to => "sessions#new", as: :sign_in
     get "sign_up", :to => "devise/registrations#new", as: :sign_up
     get "sign_out", :to => "devise/sessions#destroy", as: :sign_out
   end
-  
+    
   resources :users do
     resources :feed_entries
     resources :app_keys do
